cb6464e72a824f213673fe3dec00ce80961e3381
Emanuel995 <emanuel_cehc_95@hotmail.com>
2021-11-02 16:08:58 -0300

    crear app y modelo

--- django_ecom/app1/models.py
+++ django_ecom/app1/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db.models.base import Model
+from django.db.models.fields import CharField
 
 # Create your models here.
+
+
+class Productos (models.Model):
+    nombre = models.CharField(null=True, blank=True, max_length=150)
